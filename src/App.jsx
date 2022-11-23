63b5473139ebbd8d23e2f5e3a6589f51fda3c40d
Dwayne Paisley-Marshall <superdwayne@gmail.com>
2022-11-23 17:41:22 +0100

    Pushing changes so Yoshi can launch a vercel

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,51 @@ import {
   extend,
 } from "@react-three/fiber";
 
+import { BloomPass } from "three/examples/jsm/postprocessing/BloomPass";
+import { GlitchPass } from "three/examples/jsm/postprocessing/GlitchPass";
+
 import {
   Scroll,
   ScrollControls,
-  useScroll,
-  Sky,
   Stars,
   Lathe,
   OrbitControls,
+  Effects,
+  Image
 } from "@react-three/drei";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./App.css";
+extend({GlitchPass, BloomPass });
 
-function Joinus() {
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+
   return (
-    <mesh castShadow receiveShadow position={[0, 0, 50]} scale={[10, 50, 100]}>
-      <boxBufferGeometry />
-      <meshLambertMaterial wireframe color="white" />
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={[0.5, 0.1, 0,1]}
+      >
+      <boxGeometry args={[10, 0.5, 0]} />
+      <meshStandardMaterial color="orange"   />
     </mesh>
-  );
+  )
 }
+
+function Imagemap() {
+  const ref = useRef()
+  useFrame(() => {
+    ref.current.material.zoom = 1 // 1 and higher
+    ref.current.material.grayscale = 0 // between 0 and 1
+    ref.current.material.color.set(0x7289da) // mix-in color
+  })
+  return <Image ref={ref} position={[0,-5,0]} scale={20} transparent url="./images/icon_clyde_white_RGB.png" />
+}
+
+
+
 
 function LatheScene() {
   const points = React.useMemo(() => {
@@ -60,7 +84,7 @@ function App() {
         <section className="date">{date}</section>
 
         <section>
-          <hr />
+   
         </section>
 
         <section>
@@ -153,8 +177,10 @@ function App() {
       </main>
       <footer>
         <Canvas
+        
           gl={{ alpha: true }}
-          camera={{ position: [0, 0, 10] }}
+          camera={{ fov: 55, near: 0.1, far: 1000, position: [0, 0, 40]}}
+          jkhjhm
           style={{
             backgroundColor: "black",
             display: "block",
@@ -162,12 +188,24 @@ function App() {
             width: "100vw",
           }}
         >
+        
           <ambientLight />
-          <Joinus />
+          <Effects multisamping={8} >
+      
+      {/* <bloomPass  /> */}
+      <glitchPass attachArray="passes" />
+    </Effects>
           <OrbitControls />
           <gridHelper
-            rotation={[0.7, 1.6, 0.9]}
-            args={[60, 40, -40, "white"]}
+            rotation={[0.8, 1.58, 2.26]}
+            args={[160, 150, 20, "white"]}
+            position={[1, 0, -2]}
+          />
+         <Imagemap/>
+         <gridHelper
+            rotation={[0.8, 1.58, 2.5]}
+            args={[160, 50, 20, "white"]}
+            position={[0, -20, -2]}
           />
         </Canvas>
       </footer>
